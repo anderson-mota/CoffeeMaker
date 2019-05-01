d25a59f362aed8fe3ce6806883805fef0a0de6ad
Anderson Oliveira <anderson.oliveira@locaweb.com.br>
2019-05-01 15:05:23 -0300

    Config unit tests

---
+++ tests/CoffeeMachineTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class CoffeeMachineTest extends TestCase
+{
+    public function testMakeCoffee()
+    {
+        $this->assertEquals(1, 4-3);
+    }
+}

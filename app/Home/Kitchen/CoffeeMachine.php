5630868b291005b2e5b2e5888234a5a87506b2a8
Anderson Oliveira <anderson.oliveira@locaweb.com.br>
2019-05-02 14:11:37 -0300

    Add error

--- app/Home/Kitchen/CoffeeMachine.php
+++ app/Home/Kitchen/CoffeeMachine.php
@@ -53,7 +53,7 @@ class CoffeeMachine extends HomeAppliances
         }
     }
 
-    public function setIntensity(int $intensity) : CoffeeMachine
+    public function setIntensity(array $intensity) : CoffeeMachine
     {
         if (!in_array($intensity, self::ALLOW_INTENSITIES)) {
             throw new \Exception('Level not allow', 1);
@@ -64,7 +64,7 @@ class CoffeeMachine extends HomeAppliances
         return $this;
     }
 
-    public function grindCoffeeBeans(CoffeeBeans $coffeeBeans, CoffeeGrinder $coffeeGrinder)
+    public function grindCoffeeBeans(CoffeeBeans $coffeeBeans, CoffeeGrinder $coffeeGrinder) : CoffeeMachine
     {
         $coffeeBeans = $coffeeBeans->takeBeansByIntensity($this->intensity);
         $groundCoffee = $coffeeGrinder->grind($coffeeBeans);
